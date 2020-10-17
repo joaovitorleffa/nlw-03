01f978e865ad88e4fc49d2bc63e4cef1aea4c8b6
joaovitorleffa <joaovitorleffa02@gmail.com>
2020-10-17 11:04:38 -0300

    backend

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import 'express-async-errors';
+import errorHandler from './errors/handler';
+
+import './database/connection';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.listen(3333);
